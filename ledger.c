48b85d64d32d8fd3d098d99f99c8c8a7101a47e2
redpole-xyz <mail@redpole.xyz>
2026-01-09 10:38:28 -0600

    prev

--- ledger.c
+++ ledger.c
@@ -1,5 +1,22 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include "defs.h"
 
-void test() {
-    printf("Hello world!\n");
+int ledger_open(char * path, Ledger * ledger) {
+    return 0;
 }
+
+int ledger_close(char * path, Ledger * ledger) {
+    return 0;
+}
+
+// Make sure all data within entry is consecutive to ease saving
+Entry * ledger_add(Entry entry) {
+    return 0;
+}
+
+Entry * ledger_del(Entry entry) {
+    return 0;
+}
+
