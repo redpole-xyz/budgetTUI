efa606af52c3e6d4b8e9b1d462a4de696b489ba6
redpole-xyz <mail@redpole.xyz>
2026-01-09 20:57:06 -0600

    new_entry complete

--- menu.c
+++ menu.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdbool.h>
 #include "log_err.h"
+#include "new_entry"
 
 typedef enum {
     CMD_QUIT,
@@ -29,10 +30,84 @@ bool menu() {
             return false;
             break;
         case CMD_NEW_ENTRY: 
+            new_entry();
             return true;
             break;
         default:
             printf("Unrecognized command\n");
             return true;
     }
+}
+
+
+new_entry() {
+    char * str = (char *)calloc(256, sizeof(char));
+    Entry *entry = (Entry *)calloc(1, sizeof(Entry));
+    printf("\nNew Entry\n");
+    // Get amount
+    while (1) {
+        printf("AMOUNT: $");
+        if (fgets(str, 256, stdin) == NULL) {
+            log_err("bad input");
+        }
+        entry->amount = strtod(str, NULL);
+        if (entry->amount >= 0.01 && entry->amount < 100000000000) {
+            break;
+        }
+    }
+    // Get category
+    while (1) {
+        printf("Catagory menu\n"); 
+        printf("0: Housing\n");
+        printf("1: Groceries\n");
+        printf("2: Transit\n");
+        printf("3: Subscriptions\n");
+        printf("4: Shopping\n");
+        printf("5: Social\n");
+        printf("6: Projects\n");
+        printf("Select a category: \n");
+        scanf("%d", &entry->category);
+        getchar();
+        if (entry->category >= 0 && entry->category <= 6) {
+            break;
+        } else {
+            printf("Invalid entry\n");
+        }
+    }
+    // Get date
+    char * date_str;
+    while (success != true) {
+        printf("Press ENTER for today's date\n");
+        printf("Format: YY-MM-DD (dashes optional)\n");
+        if (fgets(str, 256, stdin) == NULL) {
+            log_err("bad input");
+        }
+        time_t t = time(NULL);
+        entry->date = *localtime(&t);
+        if (date_str == NULL) {
+            break;
+        }
+        if (strptime(date_str, "%d", &entry->date) != NULL) {
+            break;
+        }
+        if (strptime(date_str, "%Y-%m-%d", &entry->date) != NULL) {
+            break;
+        } 
+        if (strptime(date_str, "%Y%m%d", &entry->date) != NULL) {
+            break;
+        } 
+        if (date_str[0] == 'q') {
+            printf("discarding entry\n");
+            return NULL;
+        }
+        printf("failed to parse string\n");
+    }
+    // Get desc
+    printf("Description: (enter to skip)\n");
+    if (fgets(str, 256, stdin) == NULL) {
+        log_err("bad input");
+    }
+    entry->desc = str;
+    ledger_add(entry);
+    free(str);
 }
